441abe11c69a909ae0a2e23e0be703d27411299e
vinsssten <dan.wall@mail.ru>
2021-10-20 17:02:58 +0700

    Добавил все необходимое для логина, авторизации и аутентификации

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const {secret} = require('../modules/config')
+
+module.exports = function (req, res, next) {
+    if (req.method === "OPTIONS") {
+        next();
+    }
+
+    try {
+        console.log('method', req.method)
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            return res.status(403).json({message: 'Пользователь не авторизован'})
+        }
+
+        const decodedData = jwt.verify(token, secret)
+        // req.user = decodedData;
+        next();
+
+    } catch (e) {
+        console.log('отработал catch', e)
+        return res.status(403).json({message: "Пользователь не авторизован"})
+    }
+}
