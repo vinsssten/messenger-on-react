2fe9432cb554b03da41677381977cc076d7063fb
vinsssten <dan.wall@mail.ru>
2021-11-10 22:28:15 +0700

    Добавил поле ввода сообщения и кнопку отправки

--- src/components/ChatModal/components/InputCardChatModal.js
+++ src/components/ChatModal/components/InputCardChatModal.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
 function InputCardChatModal() {
+    //TODO: Сделать увеличение text-area при большом количестве символов
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="chatModal_InputMessageContainer">
+                <textarea
+                    className="chatModal_MessageInput"
+                    type="text"
+                    placeholder="Input message..."
+                />
+            </div>
+            <div className="chatModal_SendButtonContainer">
+                <div 
+                    className="chatModal_SendButton"
+                    onClick={() => {console.log('click')}}
+                    >
+                    ✉
+                </div>
+            </div>
+        </>
     )
 }
 
