fc91d7ad4003d4e2f1831917b0e6f9b2c2cefd01
vinsssten <79782423+vinsssten@users.noreply.github.com>
2021-11-10 18:07:39 +0300

    Добавил react key и переименовал переменную

--- src/components/ChatModal/components/MessageList.js
+++ src/components/ChatModal/components/MessageList.js
@@ -5,15 +5,17 @@ function MessageList ({messages}) {
 
     //TODO: Исправить отображение сообщений в неправильном порядке
     function renderMessages () {
-        return messages.map((value, index) => {
+        return messages.map((item, index) => {
             const defaultClassname = 'chatModal_MessageList-MessageLine ';
-            const finalClassname = value.isClientMessage ? defaultClassname + "rightSideMessage" : defaultClassname + "leftSideMessage";
+            const finalClassname = item.isClientMessage ? defaultClassname + "rightSideMessage" : defaultClassname + "leftSideMessage";
 
             return (
-            <div className={finalClassname}>
+            <div 
+                className={finalClassname}
+                key={index}>
                 <MessageCard
-                    isLeftSide={!value.isClientMessage}
-                    text={value.textMessage}
+                    isLeftSide={!item.isClientMessage}
+                    text={item.textMessage}
                     />
             </div>
         )})
