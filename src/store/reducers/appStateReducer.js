d7740e8abe43bbdea2be4d30c26b35657cd94a8a
vinsssten <dan.wall@mail.ru>
2021-11-11 19:45:15 +0700

    Добавил экшены и состояние модального окна, Добавил состояние ожидания подтверждения

--- src/store/reducers/appStateReducer.js
+++ src/store/reducers/appStateReducer.js
@@ -1,5 +1,8 @@
 const defaultState = {
     notificationList: [],
+    findModalActive: false,
+    waitConfirmationChat: false,
+    messageConfirmation: null,
 }
 
 const appStateReducer = (state = defaultState, action) => {
@@ -9,6 +12,14 @@ const appStateReducer = (state = defaultState, action) => {
                 sender: action.userReqId,
                 nickname: action.nickname}]
             }
+        case 'SET_WAITCONFIRMATION':
+            return {...state, waitConfirmationChat: true}
+        case 'DELETE_WAITCONFIRMATION':
+            return {...state, waitConfirmationChat: false, messageConfirmation: action.messageConfirmation}
+        case 'OPEN_FINDMODAL':
+            return {...state, findModalActive: true}
+        case 'CLOSE_FINDMODAL':
+            return {...state, findModalActive: false, messageConfirmation: null}
         default:
             return state
     }
