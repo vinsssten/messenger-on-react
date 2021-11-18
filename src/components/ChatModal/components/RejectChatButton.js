06982f097305ae27627c12d79bec1a7b5594f385
vinsssten <dan.wall@mail.ru>
2021-11-18 21:22:31 +0700

    Фикс

--- src/components/ChatModal/components/RejectChatButton.js
+++ src/components/ChatModal/components/RejectChatButton.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import '../ChatModal.css'
 
 function RejectChatButton({isRejected}) {
-    const {socket} = useSelector(state => state.app)
+    const {socket} = useSelector(state => state.socket)
 
     function rejectChat () {
         if (window.confirm('Are you sure to close chat?')) {
