7f6c300289444b7bf78c5cf629cfbd08d2036530
vinsssten <dan.wall@mail.ru>
2021-11-20 21:37:29 +0700

    Изменил верстку для titlebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Provider } from "react-redux";
-import store from "./store/store";
+import './index.css'
+
 import SocketDispatcher from "./components/MainPage/SocketDispatcher";
+import TitleBarElectron from "./components/TitleBarElectron/TitleBarElectron";
+import store from "./store/store";
+
 
 function App () {
     return (
         <Provider store={store}>
-            <SocketDispatcher />
+            <div className="container">
+                <TitleBarElectron />
+                <SocketDispatcher />
+            </div>
         </Provider>
     )
 }
